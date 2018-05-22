544d4372870ac5c9952fabdfbaf9e8551991ee88
Ron Rith <ronrith@Rons-Mac.local>
2018-05-22 15:58:24 +0700

    add basic combobox

--- combobox/ViewController.swift
+++ combobox/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var tropDown: UIPickerView!
+    
+    var list = ["1one","2two","3three"]
+    var pickerView = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        textBox.inputView = pickerView
+        textBox.textAlignment = .center
+        textBox.placeholder = "Select List"
+        
+    
     }
-
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textBox.text = list[row]
+        textBox.resignFirstResponder()
+    }
+    
+    
 
 }
 
